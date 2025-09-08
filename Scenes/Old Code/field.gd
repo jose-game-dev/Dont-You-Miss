fc0b0261b84739494caa73b4a198369e803e8143
Jose-web-dev <jose.colon193@upr.edu>
2025-09-07 21:49:41 -0400

    Release commit

--- Scenes/Old Code/field.gd
+++ Scenes/Old Code/field.gd
@@ -2,30 +2,24 @@ extends Node2D
 @onready var bullet_node: Label = $CanvasLayer/bullet
 @onready var Round_node: Label = $CanvasLayer/round
 @onready var score_node: Label = $CanvasLayer/score
-@onready var music: AudioStreamPlayer = $"Music"
 
-var bullet_count = 12:
+var bullet_count = 6:
 	set(value):
-		if value > 0:
+		if value >= 0:
 			bullet_count = value
-			bullet_node.text = "BULLETS: " + str(value)
+			bullet_node.text = str(value)
 		else:
-			bullet_count = 0
-			bullet_node.text = "BULLETS: 0"
 			_lose()
-
 
 var Round = 1:
 	set(value):
-		#Round_node.text = "ROUND: "
 		Round = value
-		Round_node.text = "ROUND: " + str(value)
+		Round_node.text = str(value)
 
 var score = 0:
 	set(value):
-		#score_node.text = "SCORE: "
 		score = value
-		score_node.text = "SCORE: " + str(value)
+		score_node.text = str(value)
 
 # We preload enemies to work with them better
 @export var ground_ninja_node: PackedScene = preload("res://Scenes/Ground_ninja.tscn")
@@ -40,16 +34,16 @@ var in_round_transitions := false
 func _ready():
 	_default_display()
 	_spawn_enemy()
-	#play music
-	music.play()
 
 func _default_display():
-	bullet_node.text = "BULLETS: " + str(bullet_count)
-	Round_node.text = "ROUND: " + str(Round)
-	score_node.text = "SCORE: " + str(score)
+	bullet_node.text = str(bullet_count)
+	Round_node.text = str(Round)
+	score_node.text = str(score)
 
 func _spawn_enemy():
 	in_round_transitions = false
+	alive_enemies = 0
+	
 	var mob_pool := [ground_ninja_node, minion_demon_node, cyclops_demon_node]
 	
 	for i in range(next_round_enemy_count):
@@ -65,27 +59,28 @@ func _spawn_enemy():
 		
 		add_child(enemy)
 		alive_enemies += 1
-
+		
 func _on_enemy_dead():
+	
 	if in_round_transitions:
 		return 
-		
+	
 	alive_enemies -= 1
-	score += 100
+	score += 10
 	
 	if alive_enemies == 0:
 		in_round_transitions = true 
-		
+#		Progression: 4 -> 6 -> 8 -> 10 -> ...
 		if Round == 1:
-			next_round_enemy_count = 3
+			next_round_enemy_count = 4
 		else:
-			next_round_enemy_count = min(10, next_round_enemy_count + 1)
-		
+			next_round_enemy_count += 1
+	
 		Round += 1
-		bullet_count = 12
-		await get_tree().create_timer(1).timeout
+		bullet_count = 6
+		
+		await get_tree().create_timer(1.0).timeout
 		_spawn_enemy()
-
 
 func _input(event):
 	if event.is_action_pressed("shoot"):
@@ -93,10 +88,11 @@ func _input(event):
 
 func _lose():
 	if $Timer.is_stopped():
+		find_child("Laugh_mob").dog_laugh()
 		for enemy in get_tree().get_current_scene().get_children():
 			if "input_pickable" in enemy:
 				enemy.input_pickable = false
 		$Timer.start()
 
 func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://UI/MainMenu.tscn")
+	get_tree().reload_current_scene()
